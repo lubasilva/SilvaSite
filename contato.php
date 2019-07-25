1e89341fa65f9170b292e2acd4a1aff32a2ee231
Lucas <lubapisilva@gmail.com>
2019-07-25 09:19:46 -0300

    quase

--- contato.php
+++ contato.php
@@ -31,6 +31,24 @@
     </div>
 
 </div>
+    
+<div class="localizacao container">
+             
+    <div class="row">
+        
+        <div class="endereco col offset-1">
+            <h2> Rua Dom Abel</h2>
+            <h2> Nº: 999</h2>
+            <h2> Bairro Dom Abel</h2>
+            <h2> CEP: 75800000</h2>
+            <h2> Jatai - GO</h2>
+        </div>
+        <div classe ="mapa col">
+        <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d7594.797965084214!2d-51.715071878233864!3d-17.866787977790484!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1spt-BR!2sbr!4v1564055819834!5m2!1spt-BR!2sbr" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+        </div>
+    </div>
+
+</div>
 
 <?php
     include("footer.php");
